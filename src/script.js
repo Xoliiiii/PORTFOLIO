1e39259800089289856bd17be8f3a57c8ebf8ae7
Xolisa <109094089+Xoliiiii@users.noreply.github.com>
2023-10-14 18:40:40 +0200

    adding function to contact form

--- src/script.js
+++ src/script.js
@@ -1,5 +1,16 @@
-// carousel for images
-let Username = docuument.getElementById("name.value")
-function showAlert(){
-    alert(`We'll be in touch soon ${Username}`);
+function showMessage(){
+      alert(`Hello ${userName}! We'll be in touch soon!`);
 }
+
+
+function tellUser(event){
+  event.preventDefault();
+  
+  let userNameInput = document.getElementById("name");
+  let userName = userNameInput.value;
+
+  showMessage(userName);
+}
+var form = document.getElementById("myForm;")
+document.getElementById("formbtn").addEventListener("click", tellUser);
+
