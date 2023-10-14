c7a23290603fa3d0ec98377bd81d63542e769921
Xolisa <109094089+Xoliiiii@users.noreply.github.com>
2023-10-14 18:41:11 +0200

    removing function

--- src/script.js
+++ src/script.js
@@ -1,16 +1,1 @@
-function showMessage(){
-      alert(`Hello ${userName}! We'll be in touch soon!`);
-}
-
-
-function tellUser(event){
-  event.preventDefault();
-  
-  let userNameInput = document.getElementById("name");
-  let userName = userNameInput.value;
-
-  showMessage(userName);
-}
-var form = document.getElementById("myForm;")
-document.getElementById("formbtn").addEventListener("click", tellUser);
 
