11f9d0d362f1bb4b56d01fc44b6a0dc4329fa583
Xolisa <109094089+Xoliiiii@users.noreply.github.com>
2023-10-15 18:12:35 +0200

    adding functionality to form and footer update

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
+function contactUs (event){
+    event.preventDefault();
+    let input = document.querySelector("#name");
+    console.log(input.valure);
+}
 
+
+
+
+
+let form = document.querySelector ("form");
+form.addEventListener('submit',contactUs);
